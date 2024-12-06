a7652e5588a98c5771d429da350b6ce67dc589d9
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-12-06 20:38:05 -0300

    use artist skeleton

--- src/app/components/artists-list/ArtistsList.tsx
+++ src/app/components/artists-list/ArtistsList.tsx
@@ -1,34 +1,51 @@
 'use client'
+import { useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
+import Image from 'next/image'
+import { ArtistSkeleton } from '@/components'
 import { Artist } from '@/interfaces'
 import { getArtists } from '@/service'
 import { larifyStore } from '@/store'
-import Image from 'next/image'
-import { useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
 
 export const ArtistsList = () => {
   const params = useSearchParams();
 
-  const { artist, artists, setArtists, setArtist } = larifyStore()
+  const { artist, artists, setArtists, setArtist, loading, isLoading, notLoading } = larifyStore()
 
   const query = params.get('artist')
 
-  const updateArtists = async() =>{
+  const updateArtists = async () => {
+    loading()
     const newArtists = await getArtists(query)
     setArtists(newArtists)
     setArtist(query || '')
+    notLoading()
   }
 
   useEffect(() => {
     updateArtists();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query])
+
+  useEffect(() => {
+    notLoading()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="data flex flex-col justify-center items-center">
       <h3 className="text-4xl my-10 capitalize font-bold">
-        { artist }
+        {artist}
       </h3>
-      {artists.length > 0 && (
+      {isLoading ? (
+        <div className="artists">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <ArtistSkeleton key={index} />
+            ))}
+        </div>
+      ) : artists.length > 0 ? (
         <div className="artists">
           {artists.map((artist: Artist, index) => (
             <div className="artist" key={index}>
@@ -44,6 +61,8 @@ export const ArtistsList = () => {
             </div>
           ))}
         </div>
+      ) : (
+        <p>No se encontraron artistas</p>
       )}
     </div>
   )
