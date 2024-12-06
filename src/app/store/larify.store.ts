ce557d89c8870836565f5b14f433adcbf29e178c
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-12-06 20:38:29 -0300

    add loading to store

--- src/app/store/larify.store.ts
+++ src/app/store/larify.store.ts
@@ -2,13 +2,17 @@ import { create } from 'zustand'
 import { Artist } from '../interfaces'
 
 type Store = {
+  isLoading: boolean,
   artist: string,
   artists: Artist[],
-  setArtists: (artists:Artist[]) => void
-  setArtist: (artists:string) => void
+  setArtists: (artists:Artist[]) => void,
+  setArtist: (artists:string) => void,
+  loading: () => void,
+  notLoading: () => void,
 }
 
 export const larifyStore = create<Store>()((set) => ({
+  isLoading: true,
   artist: '',
   artists: [],
 
@@ -19,5 +23,12 @@ export const larifyStore = create<Store>()((set) => ({
   },
   setArtist: (artist:string) => {
     set({ artist })
-  }
+  },
+  loading: () => {
+    set({ isLoading: true })
+  },
+  notLoading: () => {
+    set({ isLoading: false })
+  },
+
 }))
