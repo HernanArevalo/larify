0849505abb5a4f688f13c3bbef772663cb6a3d5f
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-12-07 20:11:41 -0300

    configure firebase

---
+++ src/app/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: "larify.firebaseapp.com",
+  projectId: "larify",
+  storageBucket: "larify.firebasestorage.app",
+  messagingSenderId: "920401292658",
+  appId: "1:920401292658:web:8a9f8e32266dd0f97081eb"
+};
+
+// Initialize Firebase
+export const FirebaseApp  = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth( FirebaseApp );
+export const FirebaseDB   = getFirestore( FirebaseApp );
