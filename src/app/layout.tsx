cd8cf623b8c003f0a1adbf86b99c8ce9ce9d101b
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-12-07 21:13:07 -0300

    update preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,33 @@
-import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import 'animate.css';
 import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Larify",
-  description: "Official Larify Website",
+  description: "Official Larify App",
+  openGraph: {
+    title: "Larify",
+    description: "Official Larify App",
+    url: "https://larify.vercel.ap",
+    siteName: "Larify",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dabmixcta/image/upload/v1733616681/jzn73nindckkxwogisfw.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Larify Preview Image",
+      },
+    ],
+    locale: "es_MX",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Larify",
+    description: "Official Larify App",
+    images: ["https://res.cloudinary.com/dabmixcta/image/upload/v1733616681/jzn73nindckkxwogisfw.jpg"],
+  },
 };
 
 const montserrat = Montserrat({ subsets: ['latin'] })
