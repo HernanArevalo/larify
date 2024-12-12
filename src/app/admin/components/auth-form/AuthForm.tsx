56ae63aaf51bfc3c412c23fa1e58a16d5da16a0d
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-12-12 13:29:08 -0300

    add admin page

---
+++ src/app/admin/components/auth-form/AuthForm.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { larifyStore } from '@/store';
+import { useState } from 'react'
+
+export const AuthForm = () => {
+  const [password, setPassword] = useState<string>('');
+
+  const { setAdmin } = larifyStore()
+
+  const checkPassword = () => {
+    if (password == '7') {
+      setAdmin(true)
+    }
+  }
+
+  return (
+    <form onSubmit={checkPassword} className="check-form">
+              <label>Ingrese la contraseña:</label>
+              <input type="password" value={password} onChange={e => { setPassword(e.target.value) }} />
+              <button type='submit'>Ingresar</button>
+            </form>
+  )
+}
